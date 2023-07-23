0e3e8cee98153375585f440bdd3cdcc0d3302456
D1eqo <136910087+D1eqo@users.noreply.github.com>
2023-07-23 10:32:48 -0300

    Create Remove Duplicates from Sorted List 83.c

---
+++ Remove Duplicates from Sorted List 83.c
@@ -0,0 +1,14 @@
+struct ListNode* deleteDuplicates(struct ListNode* head){
+    struct ListNode* noDupList = head;
+    struct ListNode* temp = head;
+
+    while (temp != NULL && temp->next != NULL) {
+        if (temp->val == temp->next->val) {
+            temp->next = temp->next->next;
+        } else {
+            temp = temp->next;
+        }
+    }
+    
+    return noDupList;
+}
